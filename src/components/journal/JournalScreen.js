ede4e190800c3ebd5f9c2062ff75c7b05e7c1e35
JosueXA <josue.xicotencal.a@gmail.com>
2021-04-08 11:58:46 -0500

    visual changed

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
-//import { NothingSelected } from './NothingSelected';
+import { NothingSelected } from './NothingSelected';
 import { NoteScreen } from '../notes/NoteScreen';
+import { useSelector } from 'react-redux';
 
 export const JournalScreen = () => {
+
+  const { active } = useSelector( state => state.notes );
+
   return (
     <div className="journal__main-content">
       
@@ -11,8 +15,11 @@ export const JournalScreen = () => {
 
       <main>
 
-        {/* <NothingSelected /> */}
-        <NoteScreen />
+        {
+          (active)
+            ? ( <NoteScreen /> )
+            : ( <NothingSelected /> )
+        }
 
       </main>
 
